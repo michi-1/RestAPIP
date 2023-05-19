628f38cd489ac51a6bf09c7603413f7e5def3139
michi-1 <reiter.michael@orotonmail.com>
2023-05-19 11:31:43 +0200

    Test

--- src/main/java/com/project/restapip/APIController.java
+++ src/main/java/com/project/restapip/APIController.java
@@ -1,11 +1,16 @@
 package com.project.restapip;
 import org.springframework.web.bind.annotation.*;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 
 @RestController
 public class APIController {
+Send s1 = new Send();
     @PostMapping("/post/{id}")
-    public String post(@RequestBody String id) {
-        System.out.println(id);
+    public String post(@RequestBody String id2, @PathVariable String id) {
+        System.out.println(id2);
+        s1.sender(id);
         return "Hello, " + id + "!";
     }
     @GetMapping("/get/{id}")
